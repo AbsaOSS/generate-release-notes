ab5dc0f5bb445725f6f773c1d6f5e64d26faebf0
miroslavpojer <miroslav.pojer@absa.africa>
2024-01-03 11:18:53 +0100

    Debugging.

--- scripts/generate-release-notes.js
+++ scripts/generate-release-notes.js
@@ -216,12 +216,18 @@ function parseChaptersJson(chaptersJson) {
 async function fetchClosedIssues(octokit, repoOwner, repoName, latestRelease) {
     console.log(`Fetching closed issues since ${latestRelease.created_at}`);
 
-    return await octokit.rest.issues.listForRepo({
+    const closedIssues = await octokit.rest.issues.listForRepo({
         owner: repoOwner,
         repo: repoName,
         state: 'closed',
         since: new Date(latestRelease.created_at)
-    }).filter(issue => !issue.pull_request).reverse();
+    });
+    console.log(`Found ${closedIssues.data.length} closed issues since last release`);
+
+    const onlyIssues = closedIssues.filter(issue => !issue.pull_request).reverse();
+    console.log(`Found ${onlyIssues.length} closed issues (only Issues) since last release`);
+
+    return onlyIssues;
 }
 
 /**
