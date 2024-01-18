4535ed60b8adc2bc485b1fdf1a5832b7d99b89bb
miroslavpojer <miroslav.pojer@absa.africa>
2024-01-18 08:41:52 +0100

    Updated titles and defaults messages of warning chapters.

--- dist/index.js
+++ dist/index.js
@@ -31339,11 +31339,11 @@ async function run() {
         });
 
         if (warnings) {
-            releaseNotes += "### Issues without Pull Request ⚠️\n" + (issuesWithoutPR || "All issues linked to a Pull Request.") + "\n\n";
-            releaseNotes += "### Issues without User Defined Labels ⚠️\n" + (issuesWithoutUserLabels || "All issues contain at least one of user defined labels.") + "\n\n";
-            releaseNotes += "### Issues without Release Notes ⚠️\n" + (issuesWithoutReleaseNotes || "All issues have release notes.") + "\n\n";
-            releaseNotes += "### PRs without Linked Issue ⚠️\n" + (prsWithoutLinkedIssue || "All PRs are linked to issues.") + "\n\n";
-            releaseNotes += "### PRs Linked to Open Issue ⚠️\n" + (prsLinkedToOpenIssue || "All PRs are linked to Closed issues.") + "\n\n";
+            releaseNotes += "### Closed Issues without Pull Request ⚠️\n" + (issuesWithoutPR || "All closed issues linked to a Pull Request.") + "\n\n";
+            releaseNotes += "### Closed Issues without User Defined Labels ⚠️\n" + (issuesWithoutUserLabels || "All closed issues contain at least one of user defined labels.") + "\n\n";
+            releaseNotes += "### Closed Issues without Release Notes ⚠️\n" + (issuesWithoutReleaseNotes || "All closed issues have release notes.") + "\n\n";
+            releaseNotes += "### Merged PRs without Linked Issue ⚠️\n" + (prsWithoutLinkedIssue || "All merged PRs are linked to issues.") + "\n\n";
+            releaseNotes += "### Merged PRs Linked to Open Issue ⚠️\n" + (prsLinkedToOpenIssue || "All merged PRs are linked to Closed issues.") + "\n\n";
         }
         releaseNotes += "#### Full Changelog\n" + changelogUrl;
 
