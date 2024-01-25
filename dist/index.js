b0c719f639f225e010a41385d9569926a50278ed
miroslavpojer <miroslav.pojer@absa.africa>
2024-01-25 13:39:37 +0100

    - Code review and clean up.

--- dist/index.js
+++ dist/index.js
@@ -29118,12 +29118,13 @@ async function getPRCommitAuthors(octokit, repoOwner, repoName, relatedPRs) {
                     if (emailMatch && nameMatch) {
                         const email = emailMatch[1];
                         const name = nameMatch[1].trim();
+
                         console.log(`Searching for GitHub user with email: ${email}`);
+
                         const searchResult = await octokit.rest.search.users({
                             q: `${email} in:email`
                         });
-                        console.log(searchResult)
-                        console.log(searchResult.data.items)
+
                         const user = searchResult.data.items[0];
                         if (user && user.login) {
                             commitAuthors.add('@' + user.login);
@@ -29173,7 +29174,6 @@ async function getReleaseNotesFromComments(octokit, issueNumber, issueTitle, iss
     let releaseNotes = [];
     for (const comment of comments.data) {
         if (comment.body.toLowerCase().startsWith('release notes')) {
-            // const noteContent = comment.body.replace(/^release notes\s*/i, '').trim();
             const noteContent = comment.body.replace(/^release notes:?.*(\r\n|\n|\r)?/i, '').trim();
             console.log(`Found release notes in comments for issue #${issueNumber}`);
             releaseNotes.push(noteContent.replace(/^\s*[\r\n]/gm, '').replace(/^/gm, '  '));
@@ -29600,6 +29600,7 @@ module.exports.run = run;
 if (require.main === require.cache[eval('__filename')]) {
     run();
 }
+
 
 /***/ }),
 
