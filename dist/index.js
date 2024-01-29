9a1a5b92d4a07bd3567fab22d58646a525cdf6f5
miroslavpojer <miroslav.pojer@absa.africa>
2024-01-29 15:19:08 +0100

    - Simplified duplicate string as GitHub does not support colors.

--- dist/index.js
+++ dist/index.js
@@ -29509,7 +29509,7 @@ async function run() {
     const githubToken = process.env.GITHUB_TOKEN;
     const tagName = core.getInput('tag-name');
     const githubRepository = process.env.GITHUB_REPOSITORY;
-    const duplicate = "- **<span style=\"color: red;\">[Duplicate]<span>** #";
+    const duplicate = "- _**<span style=\"color: red;\">[Duplicate]<span>**_ #";
 
     // Validate GitHub token
     if (!githubToken) {
