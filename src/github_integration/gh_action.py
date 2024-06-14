db1807447a30f17a3d1ca809bf4322c118de8c2e
miroslavpojer <miroslav.pojer@absa.africa>
2024-06-14 14:09:36 +0200

    - Changes from CI testing.

--- src/github_integration/gh_action.py
+++ src/github_integration/gh_action.py
@@ -9,7 +9,8 @@ def get_input(name: str) -> str:
 def set_output(name: str, value: str, default_output_path: str = "default_output.txt"):
     output_file = os.getenv('GITHUB_OUTPUT', default_output_path)
     with open(output_file, 'a') as f:
-        f.write(f'{name}={value}\n')
+        # f.write(f'{name}={value}\n')
+        f.write(f'{name}<<EOF{value}EOF\n')
 
 
 def set_failed(message: str):
